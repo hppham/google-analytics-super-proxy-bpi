006dc4ce9dfa3f6e4af9046944caa6c3efe6a14f
hpham <huy.pham@bpiedu.com>
2016-10-08 20:34:44 -0700

    init commit

---
+++ src/controllers/display_chart.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python2.7
+
+
+"""Display the public chart.
+
+These handlers are for actions performed by external users that may or may not
+be signed in. This is configured in app.yaml. Additional logic is provided by
+utility functions.
+
+  PublicQueryResponseHandler: Outputs the API response for the requested query.
+  NotAuthorizedHandler: Handles unauthorized requests.
+"""
+
+__author__ = 'huy pham'
+
+from controllers import base
+from controllers.util import co
+import webapp2
+
+
+class DisplayChart(base.BaseHandler):
+  """Redirect to the display_chart page."""
+
+  def get(self):
+    self.RenderHtmlTemplate('display_chart.html')
+
+
+app = webapp2.WSGIApplication(
+    [(co.LINKS['display_chart'], DisplayChart)],
+    debug=True)
